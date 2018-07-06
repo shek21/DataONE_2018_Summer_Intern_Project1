3683dccf8bafdc8b12e9b1877ad4412d78ff482d
Seokki Lee <slee195@hawk.iit.edu>
2018-07-06 12:57:02 -0500

    update prev + add a local tale

---
+++ tale/bundling.R
@@ -0,0 +1,79 @@
+# Set working directory
+setwd("../new/")
+
+# Create metadata in eml
+source("../createEml.R")
+
+
+# create a package
+library(dataone)
+library(datapack)
+
+dp <- new("DataPackage")
+
+
+# Add members
+metadataObj <- new("DataObject", format="eml://ecoinformatics.org/eml-2.1.1", filename="./meta.xml")
+dp <- addMember(dp, metadataObj)
+
+# Collect lists of files except metadata file
+flists <- grep(list.files("."), pattern="\\.xml$", inv=T, value=T)
+flists <- grep(flists, pattern="\\.zip$", inv=T, value=T)
+
+# Filter out directory names
+flists <- setdiff(flists, list.dirs(recursive = FALSE, full.names = FALSE))
+
+for(o in flists) { 
+  obj <- new("DataObject", filename=paste("./",o,sep=""))
+  dp <- addMember(dp, obj, metadataObj)
+}
+
+print(dp)
+
+
+# Initialize recordr
+#devtools::install_github("nceas/recordr")
+library(recordr)
+rc <- new("Recordr")
+
+# Capture provenance
+progFiles <- list.files(".", pattern="\\.R$")
+for(i in progFiles) {
+  record(rc, paste("./",i,sep=""), tag=i)
+}
+
+# Add provenance into the package
+for(i in 1:length(progFiles)) {
+  # capture file names and access types
+  vr <- viewRuns(rc, seq=i)
+  vrdf <- setNames(data.frame(basename(vr$files$filePath), vr$files$access), c("names","access"))
+  vrUsed <- setNames(subset(vrdf, vrdf$access == "read"), c("names","acess"))
+  vrDerived <- setNames(subset(vrdf, vrdf$access == "write"), c("names","acess"))
+  
+  # capture ids for sources (those files used and read)
+  sids <- NULL
+  for(n in 1:length(vrUsed$names)) {
+    sid <- selectMember(dp, name="sysmeta@fileName", value=as.character(vrUsed$names[n]))
+    sids <- c(sids, sid)
+  }
+  
+  # capture ids for outputs (those files derived)
+  oids <- NULL
+  for(o in 1:length(vrDerived$names)) {
+    oid <- selectMember(dp, name="sysmeta@fileName", value=as.character(vrDerived$names[o]))
+    oids <- c(oids, oid)
+  }
+  
+  # capture id for program script
+  pid <- selectMember(dp, name="sysmeta@fileName", value=progFiles[i])
+  
+  # create provenance relationships
+  dp <- describeWorkflow(dp, sources=sids, program=pid, derivations=oids)
+}
+
+print(dp)
+
+
+# Publish package to DataONE Test node
+#d1c <- D1Client("STAGING", "urn:node:mnTestARCTIC")
+#pkgId <- uploadDataPackage(d1c, dp, public=TRUE, quiet=FALSE)
---
+++ tale/setup.R
@@ -0,0 +1,6 @@
+# install required packages
+install.packages("rdflib")
+devtools::install_github("nceas/recordr")
+install.packages("rworldxtra")
+
+
